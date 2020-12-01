a008fe043faba80eaad7f9c85fddce61a6170e3c
Lukasz Lato <lukasz.lato@summer-solutions.com>
2020-12-01 14:25:52 +0100

    refactor

--- service/registry/request/log.go
+++ service/registry/request/log.go
@@ -6,7 +6,7 @@ import (
 	"github.com/summer-solutions/spring/service/log"
 )
 
-func LogGlobalService(provider ...log.RequestFieldProvider) spring.InitHandler {
+func LogRequestService(provider ...log.RequestFieldProvider) spring.InitHandler {
 	return func(s *spring.Server, def *spring.Def) {
 		def.Name = "log_request"
 		def.Build = func(ctn di.Container) (interface{}, error) {
