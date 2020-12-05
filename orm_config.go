f3ffa89aef9c544281f410ae52fe5e55ea33f467
Lukasz Lato <lukasz.lato@summer-solutions.com>
2020-12-05 21:03:01 +0100

    refactor

--- orm_config.go
+++ orm_config.go
@@ -1,16 +1,15 @@
-package services
+package spring
 
 import (
 	"github.com/sarulabs/di"
-	"github.com/summer-solutions/spring"
 
 	"github.com/summer-solutions/orm"
 )
 
-type RegistryInitFunc func(registry *orm.Registry)
+type ORMRegistryInitFunc func(registry *orm.Registry)
 
-func OrmRegistry(init RegistryInitFunc) *spring.ServiceDefinition {
-	return &spring.ServiceDefinition{
+func OrmRegistry(init ORMRegistryInitFunc) *ServiceDefinition {
+	return &ServiceDefinition{
 		Name:   "orm_config",
 		Global: true,
 		Build: func(ctn di.Container) (interface{}, error) {
@@ -18,7 +17,7 @@ func OrmRegistry(init RegistryInitFunc) *spring.ServiceDefinition {
 			if err != nil {
 				return nil, err
 			}
-			registry := orm.InitByYaml(configService.(*spring.ViperConfig).Get("orm").(map[string]interface{}))
+			registry := orm.InitByYaml(configService.(*ViperConfig).Get("orm").(map[string]interface{}))
 			init(registry)
 			ormConfig, err := registry.Validate()
 			return ormConfig, err
